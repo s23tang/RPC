48379411eed5872c9b7afb66ea2e924f9dd3d60b
wingzerod <wingzerod@gmail.com>
2013-06-30 17:15:17 -0400

    init V1.2

--- message.cpp
+++ message.cpp
@@ -7,12 +7,23 @@
 //
 
 #include "message.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
 
 struct Message *parseMessage(char *buf, int msgType) {
+    
+    Message *msg = new Message();
+    
     switch (msgType) {
         case REGISTER:
             
-            
+            msg->server_identifier = new char[strlen(buf) + 1];
+            strcpy(msg->server_identifier, buf);
+            msg = msg + strlen(buf) + 1;
             
             break;
         case LOC_REQUEST:
